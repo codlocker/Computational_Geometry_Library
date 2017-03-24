de1e0f2e390e37d83a7375208e64d222114312ba
codlocker <ipsit.sahoo11@gmail.com>
2017-03-24 15:41:05 +0530

    Renewed Jarvis Mrach Algo

--- JarvisMarch.h
+++ JarvisMarch.h
@@ -1,88 +1,66 @@
+//
+// Created by niranjansooda on 24/3/17.
+//
 
-//
-// Created by calvrix on 13/02/17.
-//
-#ifndef COMPUTGEOALGOS_JARVISMARCH_H
-#define COMPUTGEOALGOS_JARVISMARCH_H
+#ifndef COMPUTGEOALGOS_MYJARVIS_H
+#define COMPUTGEOALGOS_MYJARVIS_H
+
 
 #include <iostream>
 #include <bits/stdc++.h>
 #include "origin.h"
+#define ull unsigned long long /*!< Macro for making my typing life easier*/
 using namespace std;
-
-vector<int> indices_j;
-//! Generic Swap Function
-void swap(double &p, double &q) {
-	int tray = p;
-	p = q;
-	q = tray;
-}
-
-//! Point Swap Function
-/*! Swaps the coordinates of the points passed. Used for **efficiency** by reducing number of checks
- */
-void swapElements(pair<double,double> &p, pair<double, double> &q) {
-	swap(p.first,q.first);
-	swap(p.second,q.second);
-}
-
-
 //! **Jarvis March Algorithm Implementation**
 /*! 1. First Step is to sort points with respect to their x coordinates.\n
  *
  *  2. Start from the left-most point.\n
  *
  *  3. Choose and add the point that makes the largest obtuse interior angle with the latest edge on the hull.\n
- * 
- *	4. Swap the point with the corresponding nth point in the point set to avoid repeating already chosen points
  */
-double execJarvisMarch(vector<pair<double, double> > Points) {
-	int len, i, current, min = 0, hullLength=0;
-	len = int(Points.size());
-	double x_min = Points[0].first; // a **double** value which gets the point with lowest y-coordinate
-	pair<double, double> leftMost;
-    //clock_t j_taken;
-    //j_taken = clock();
-	for (int i = 1; i < len; i++)
-	{
-		if (orderedSort(Points[i], Points[min]))
-			x_min = Points[i].first, min = i;
-	}
-
-	P0 = leftMost = Points[min]; //P0 denotes the latest point permanently added to the hull
-    indices_j.pb(min);
-	swapElements(Points[hullLength++],Points[min]); //Swap the left-most point with the point in the set with index 0
-	/*
+double execJarvisMarch(vector<pair<double, double> > input){
+    vector<pair<double, double> >output;
+    vector<pair<double, double> >::iterator itr,itr2;
+    vector<pair<double, double> >::iterator itr1=input.begin();
+    vector<int>indices;
+    for(itr=input.begin()+1;itr<input.end();itr++){
+        if(itr->first<itr1->first){
+            itr1=itr;
+        }
+    }
+    itr=itr1;
+    /*
 	 * Form the Hull by processing the remaining points
 	 */
-	vector<pair<double , double> > convex_hull; // a **vector data structure** for storing the convex hull points
-	do {
-		convex_hull.pb(P0);
-		for (i = hullLength, current = i; i < len; i++) {
-			//Find the next point by comparing angles made with the last edge on the hull
-			if (orderByPolar(Points[i],Points[current])) {
-				current = i;
-			}
-		}
-		//Since the above check starts from an index = hullLength, check if the start of the hull is the next point
-		if (orderByPolar(Points[0],Points[current])) {
-			current = 0;
-		}
-		P0 = Points[current];
-		swapElements(Points[hullLength++],Points[current]); //Swap the latest added point with the point in the set having index equal to the hull length		                                                 
-	} while (P0 != leftMost);
+    do{
+        int index_position=itr-input.begin();
+        indices.push_back(index_position);
+        output.push_back(*itr);
+        if(itr==input.end())
+            itr2=input.begin();
+        itr2=(itr+1);
+        vector<pair<double, double> >::iterator itr3=input.begin();
+        while(itr3<input.end()){
+            //Find the next point by comparing angles made with the last edge on the hull
+            if(orientation(*itr,*itr3,*itr2)==3){
+                itr2=itr3;
+            }
+            itr3++;
+        }
+        itr=itr2;
+    }while(itr!=itr1);
     ofstream out_file;
     out_file.open("output.ch");
     out_file << "CH\n";
-    out_file << len << " " << convex_hull.size() << "\n";
-    for(int i=0;i < len;i++) {
-        out_file << Points[i].first << " " << Points[i].second << " 0\n";
+    out_file <<input.size()<< " " << output.size() << "\n";
+    for(int i=0;i < input.size() ;i++) {
+        out_file << input[i].first << " " << input[i].second << " 0\n";
     }
-    for(i = 0; i < convex_hull.size(); i++) {
-        out_file << i+1 << " ";
+    for(int i = 0; i <indices.size(); i++) {
+        out_file << indices[i] << " ";
     }
-	printVectorData(hullLength-1,convex_hull,"");
-    //j_taken = clock()-j_taken;
+    printVectorData(output.size(),output,"");
     return 0;
 }
-#endif
+
+#endif //COMPUTGEOALGOS_MYJARVIS_H
